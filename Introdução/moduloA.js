7d3421dcd364a3fd257a3623d4a47fb7767fe62e
alehxalves <alehxalves@gmail.com>
2022-03-21 17:23:42 -0300

    Up

---
+++ Introdução/moduloA.js
@@ -0,0 +1,3 @@
+exports.bemVindo = 'Bem vindo ao node!'
+this.ola = 'Fala Pessoal!'
+module.exports.ateLogo = 'Até o Próximo Exemplo!'
---
+++ Introdução/moduloCliente.js
@@ -0,0 +1,13 @@
+// Usando sistema de modulos do Node commonsJs
+
+const moduloA = require('./moduloA')
+const moduloB = require('./moduloB')
+
+console.log(moduloA.ola)
+console.log(moduloA.bemVindo)
+console.log(moduloA.ateLogo)
+console.log(moduloA)
+
+console.log(moduloB.bomDia)
+console.log(moduloB.boaNoite())
+console.log(moduloB)
